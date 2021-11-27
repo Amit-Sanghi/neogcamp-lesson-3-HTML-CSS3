acd50c8acbea771f9b9d3e9ba401486a58f6cc8d
Amit Kumar Sanghi <94451087+Amit-Sanghi@users.noreply.github.com>
2021-11-27 14:46:54 +0530

    update styles

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
+let radius = [3, 1, 2, 4]
 
-function x(y){
-    console.log(y)
-     y();
-       
+let area = function(radius){
+    let a = Math.PI*radius*radius;
+    return a;
 }
-x(function y(){
-    console.log("y")
-})
+let circum = function(radius){
+    let b = Math.PI*2*radius;
+    return b;
+}
 
+let calculate = function (radius, logic){
+    let output = [];
+    for (let i =0; i< radius.length; i++){
+        output.push(logic(radius[i]))
+    }
+    return output;
+}
 
-
+console.log(calculate(radius, area));
+console.log(calculate(radius, circum))
 
